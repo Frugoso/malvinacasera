78c6d3febbcc4b7b36e39db4dc582995016581f4
Frugoso <pancho1502@hotmail.com>
2020-12-11 20:01:24 -0300

    Proyecto creado

---
+++ src/main/java/com/proyecto/malvina/MalvinaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.malvina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalvinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalvinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/malvina/MalvinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.malvina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalvinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
